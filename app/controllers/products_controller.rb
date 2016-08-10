75b46180b7cec49f6c82bc04618187e9910892a3
Andrew Papacostas <a_papacostas@yahoo.com>
2016-08-10 09:18:49 +0000

    add product code and routes..testing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,35 @@
 class ProductsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :show_errors
+
+ def show
+   @product = Product.find(params[:id])
+ end
+
+ def index
+   @products = Product.all
+ end
+
+ def new
+   @product = Product.new
+ end
+
+ def create
+   @product = Product.where(name: product_params[:name].downcase).first_or_create
+   if @product.save
+     redirect_to product_path(@product)
+   else
+     render :new
+   end
+ end
+
+ private
+
+ def show_errors
+   flash[:error] = "Please select add a product."
+   redirect_to products_path
+ end
+
+ def product_params
+   params.require(:product).permit(:name)
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users
+  resources :products, only: [:index, :show, :new, :create] #testing routes
+
 
   root to: "welcome#index"
 
