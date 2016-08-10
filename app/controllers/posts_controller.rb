4122d1c9a8a73908b42478a1544fda03175275d5
Andrew Papacostas <a_papacostas@yahoo.com>
2016-08-10 18:17:06 +0000

    created posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,30 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def edit
+
+  end
+
+  def new
+    @post = Post.new
+  end
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path
+  end
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
+
 end
