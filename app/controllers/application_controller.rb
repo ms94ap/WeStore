66518d99a210ad5651612f3dd8b46545f0f747a3
Andrew Papacostas <a_papacostas@yahoo.com>
2016-08-12 19:59:46 +0000

    debug with after_sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller? #
+  before_action :configure_permitted_parameters, if: :devise_controller? #keep the params in edit.
+  
 
   protect_from_forgery with: :exception
 
   protected
 
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :country, :email])
     end
+
+    def after_sign_in_path_for(user)
+    request.env['omniauth.origin'] || stored_location_for(user) || user_path(user)
+  end
 end
