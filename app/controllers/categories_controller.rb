3c66c9ae32329cf592933de97ca81992e2c4d61f
Andrew Papacostas <a_papacostas@yahoo.com>
2016-08-13 20:06:52 +0000

    remove filter, admin-role to controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,7 @@
 class CategoriesController < ApplicationController
 layout "admin_layout"
-before_filter :authenticate_user!
-before_filter do
-  redirect_to :new_user_session_path unless current_user && current_user.admin?
+ #before_filter to be removed in version 5.1
+before_action :check_admin
 end
 
  def index
@@ -16,27 +15,29 @@ end
  def create
    @category = Category.create(:name)
    if @category.save
-     redirect_to admin_category_path(@category)
+     redirect_to admin_category_path(@category), notice: "#{@category.name} category created"
    end
  end
 
  def edit
    @category = Category.find(params[:id])
     if @category.update(name: params[:name])
-      redirect_to admin_category_path(@category), notice: "Category updated."
+      redirect_to admin_category_path(@category), notice: "#{@category.name} category updated."
     else
       render :edit
     end
   end
 
  def delete
-   @category = Category.find[params(:id)]
+   @category = Category.find(params[:id])
    @category.destroy
    redirect_to admin_category_path(@category)
  end
 
  private
- def admin?
-   self.admin == true
+ def check_admin
+   if !current_user.admin
+     redirect_to root, alert: "Admin rights required!!!"
+   end
  end
 end
