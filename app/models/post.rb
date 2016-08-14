dc43a9c3d4fe27d22fe485a2de5ea9453da636fe
Andrew Papacostas <a_papacostas@yahoo.com>
2016-08-14 16:13:25 +0000

    add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
   belongs_to :product
+  validates :name, presence: true
 
   # accepts_nested_attributes_for :product
 
   def product_attributes=(pro_attributes)
-    
+
     Product.create(name: pro_attributes["name"], minimum_quantity: pro_attributes["minimum_quantity"].to_i, price: pro_attributes["price"].to_f)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  validates :name, :minimum_quantity, :price, presence: true
 
   def category_name=(name)
     self.category = Category.find_or_create_by(name: name)
