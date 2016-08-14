7bb4aa0a26d6e258f4420d75537eb35f2960a349
Andrew Papacostas <a_papacostas@yahoo.com>
2016-08-14 16:18:47 +0000

    delete comment file

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-  
 
  def show
    @product = Product.find(params[:id])
@@ -14,22 +13,16 @@ class ProductsController < ApplicationController
  end
 
  def create
-   @product = Product.where(name: product_params[:name].downcase).first_or_create
-   if @product.save
-     redirect_to product_path(@product)
+     category = Category.find_or_create_by(name: params[:category_name])
+     Product.create(product_params, category: category)
    else
      render :new
    end
  end
 
  private
-
- def show_errors
-   flash[:error] = "Please add a product."
-   redirect_to products_path
- end
-
+ 
  def product_params
-   params.require(:product).permit(:name)
+   params.require(:product).permit(:category, :name, :minimum_quantity, :price)
  end
 end
