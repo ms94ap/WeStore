5e05131a7e813950fbf2d06256d874b9d9a70356
Andreas Papacostas <github email address>
2016-10-25 14:39:36 +0300

    add CSRF ERRORS

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,10 @@
 (function() {
-
-	angular
-		.module('app', ['ui.router', 'templates'])
-
+    'use strict';
+    
+    angular
+        .module('app', ['ui.router', 'templates'])
+        .config(function($httpProvider) {
+        // for CSRF Errors
+        $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
+        });
 }());
